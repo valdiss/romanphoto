4548c8e296119cb3ae3a55e0419b6973d6329495
Metallou <kevincastejon13@gmail.com>
2016-11-15 20:17:21 +0100

    compressed all photos

--- photos/resizer.sh
+++ photos/resizer.sh
@@ -2,74 +2,79 @@
 
 #chapitre_1
 #1095x1460
-convert chapitre_1/originals/1_01.jpg -resize 783x1045 chapitre_1/1_01.jpg
+convert chapitre_1/originals/1_01.jpg -quality 33 -resize 783x1045 chapitre_1/1_01.jpg
 #1093x1458
-convert chapitre_1/originals/1_02.jpg -resize 783x1045 chapitre_1/1_02.jpg
+convert chapitre_1/originals/1_02.jpg -quality 33 -resize 783x1045 chapitre_1/1_02.jpg
 #2316x3088
-convert chapitre_1/originals/1_03.jpg -resize 783x1045 chapitre_1/1_03.jpg
+convert chapitre_1/originals/1_03.jpg -quality 33 -resize 783x1045 chapitre_1/1_03.jpg
 #983x1312
-convert chapitre_1/originals/1_04.jpg -resize 783x1045 chapitre_1/1_04.jpg
+convert chapitre_1/originals/1_04.jpg -quality 33 -resize 783x1045 chapitre_1/1_04.jpg
 #1125x1500
-convert chapitre_1/originals/1_05.jpg -resize 783x1045 chapitre_1/1_05.jpg
+convert chapitre_1/originals/1_05.jpg -quality 33 -resize 783x1045 chapitre_1/1_05.jpg
 echo "Chapitre_1 resized"
 
 #chapitre_2
 #1500x1125
-convert chapitre_2/originals/2_01.jpg -resize 1393x1045 chapitre_2/2_01.jpg
+convert chapitre_2/originals/2_01.jpg -quality 33 -resize 1393x1045 chapitre_2/2_01.jpg
 #1125x1500
-convert chapitre_2/originals/2_02.jpg -resize 783x1045 chapitre_2/2_02.jpg
+convert chapitre_2/originals/2_02.jpg -quality 33 -resize 783x1045 chapitre_2/2_02.jpg
 #1079x1439
-convert chapitre_2/originals/2_03.jpg -resize 783x1045 chapitre_2/2_03.jpg
+convert chapitre_2/originals/2_03.jpg -quality 33 -resize 783x1045 chapitre_2/2_03.jpg
 #1125x1500
-convert chapitre_2/originals/2_04.jpg -resize 783x1045 chapitre_2/2_04.jpg
+convert chapitre_2/originals/2_04.jpg -quality 33 -resize 783x1045 chapitre_2/2_04.jpg
 #1036x1381
-convert chapitre_2/originals/2_05.jpg -resize 783x1045 chapitre_2/2_05.jpg
+convert chapitre_2/originals/2_05.jpg -quality 33 -resize 783x1045 chapitre_2/2_05.jpg
 echo "Chapitre_2 resized"
 
 #chapitre_3
 #1087x1449
-convert chapitre_3/originals/3_01.jpg -resize 783x1045 chapitre_3/3_01.jpg
+convert chapitre_3/originals/3_01.jpg -quality 33 -resize 783x1045 chapitre_3/3_01.jpg
 #1500x1125
-convert chapitre_3/originals/3_02.jpg -resize 1393x1045 chapitre_3/3_02.jpg
+convert chapitre_3/originals/3_02.jpg -quality 33 -resize 1393x1045 chapitre_3/3_02.jpg
 #1125x1500
-convert chapitre_3/originals/3_03.jpg -resize 783x1045 chapitre_3/3_03.jpg
+convert chapitre_3/originals/3_03.jpg -quality 33 -resize 783x1045 chapitre_3/3_03.jpg
 #1125x1500
-convert chapitre_3/originals/3_04.jpg -resize 783x1045 chapitre_3/3_04.jpg
+convert chapitre_3/originals/3_04.jpg -quality 33 -resize 783x1045 chapitre_3/3_04.jpg
 #1500x1125
-convert chapitre_3/originals/3_05.jpg -resize 1393x1045 chapitre_3/3_05.jpg
+convert chapitre_3/originals/3_05.jpg -quality 33 -resize 1393x1045 chapitre_3/3_05.jpg
 #1125x1500
-convert chapitre_3/originals/3_06.jpg -resize 783x1045 chapitre_3/3_06.jpg
+convert chapitre_3/originals/3_06.jpg -quality 33 -resize 783x1045 chapitre_3/3_06.jpg
 #1500x1125
-convert chapitre_3/originals/3_07.jpg -resize 1393x1045 chapitre_3/3_07.jpg
+convert chapitre_3/originals/3_07.jpg -quality 33 -resize 1393x1045 chapitre_3/3_07.jpg
+#compress only
+convert chapitre_3/originals/3_08_NB.jpg -quality 33 chapitre_3/3_08_NB.jpg
+
 echo "Chapitre_3 resized"
 
 #chapitre_4
 #1500x1126
-convert chapitre_4/originals/4_01.jpg -resize 1392x1045 chapitre_4/4_01.jpg
+convert chapitre_4/originals/4_01.jpg -quality 33 -resize 1392x1045 chapitre_4/4_01.jpg
+#compress only
+convert chapitre_4/originals/4_02.jpg -quality 33 chapitre_4/4_02.jpg
 #1500x1126
-convert chapitre_4/originals/4_03.jpg -resize 1392x1045 chapitre_4/4_03.jpg
+convert chapitre_4/originals/4_03.jpg -quality 33 -resize 1392x1045 chapitre_4/4_03.jpg
 #1500x1125
-convert chapitre_4/originals/4_04.jpg -resize 1392x1045 chapitre_4/4_04.jpg
+convert chapitre_4/originals/4_04.jpg -quality 33 -resize 1392x1045 chapitre_4/4_04.jpg
 #1500x1125
-convert chapitre_4/originals/4_05.jpg -resize 1392x1045 chapitre_4/4_05.jpg
+convert chapitre_4/originals/4_05.jpg -quality 33 -resize 1392x1045 chapitre_4/4_05.jpg
 #1500x1125
-convert chapitre_4/originals/4_05_2.jpg -resize 1392x1045 chapitre_4/4_05_2.jpg
+convert chapitre_4/originals/4_05_2.jpg -quality 33 -resize 1392x1045 chapitre_4/4_05_2.jpg
 #3264x2448
-convert chapitre_4/originals/4_06.jpg -resize 1392x1045 chapitre_4/4_06.jpg
+convert chapitre_4/originals/4_06.jpg -quality 33 -resize 1392x1045 chapitre_4/4_06.jpg
 #1931x1450
-convert chapitre_4/originals/4_06_1.jpg -resize 1392x1045 chapitre_4/4_06_1.jpg
+convert chapitre_4/originals/4_06_1.jpg -quality 33 -resize 1392x1045 chapitre_4/4_06_1.jpg
 #2262x1694
-convert chapitre_4/originals/4_06_2.jpg -resize 1392x1045 chapitre_4/4_06_2.jpg
+convert chapitre_4/originals/4_06_2.jpg -quality 33 -resize 1392x1045 chapitre_4/4_06_2.jpg
 #2594x1944
-convert chapitre_4/originals/4_06_3.jpg -resize 1392x1045 chapitre_4/4_06_3.jpg
+convert chapitre_4/originals/4_06_3.jpg -quality 33 -resize 1392x1045 chapitre_4/4_06_3.jpg
 #2931x2194
-convert chapitre_4/originals/4_06_4.jpg -resize 1392x1045 chapitre_4/4_06_4.jpg
+convert chapitre_4/originals/4_06_4.jpg -quality 33 -resize 1392x1045 chapitre_4/4_06_4.jpg
 #3264x2448
-convert chapitre_4/originals/4_07.jpg -resize 1392x1045 chapitre_4/4_07.jpg
+convert chapitre_4/originals/4_07.jpg -quality 33 -resize 1392x1045 chapitre_4/4_07.jpg
 #1500x1125
-convert chapitre_4/originals/4_08.jpg -resize 1392x1045 chapitre_4/4_08.jpg
+convert chapitre_4/originals/4_08.jpg -quality 33 -resize 1392x1045 chapitre_4/4_08.jpg
 #1920x767
-convert chapitre_4/originals/light.jpg -resize 2615x1045 chapitre_4/light.jpg
+convert chapitre_4/originals/light.jpg -quality 33 -resize 2615x1045 chapitre_4/light.jpg
 echo "Chapitre_4 resized"
 
 exit 0
